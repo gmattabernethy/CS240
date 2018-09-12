a30a5f413b39caf3a3d8233fdbcafd6aba1646e0
Glenn Matthew Abernethy <gmattabe@harrier.cs.byu.edu>
2018-09-12 13:28:32 -0600

    passed off

--- Image.java
+++ Image.java
@@ -4,21 +4,21 @@ public class Image {
 	int height;
 	int numOfPixels;
 	Pixel[][] pixels;
-	
+
 	Image(int width, int height){
 		this.width = width;
 		this.height = height;
 		this.numOfPixels = 0;
 		this.pixels = new Pixel[height][width];
 	}
-	
+
 	public void addPixel(Pixel p) {
 		int h = this.numOfPixels/this.width;
 		int w = this.numOfPixels%this.width;
 		pixels[h][w] = p;
 		this.numOfPixels++;
 	}
-	
+
 	public void invert() {
 		for(int i = 0; i < this.height; i++) {
 			for(int j = 0; j < this.width; j++) {
@@ -26,7 +26,7 @@ public class Image {
 			}
 		}
 	}
-	
+
 	public void grayscale() {
 		for(int i = 0; i < this.height; i++) {
 			for(int j = 0; j < this.width; j++) {
@@ -34,16 +34,16 @@ public class Image {
 			}
 		}
 	}
-	
+
 	public void emboss() {
 		for(int i = this.height-1; i >= 0; i--) {
 			for(int j = this.width-1; j >= 0; j--) {
 				int maxDiff = 0;
 				Pixel p = this.pixels[i][j];
 				int gray;
-				
+
 				//Be sure to account for the situation where r-1 or c-1 is less than 0. gray should be 128 in this case
-				
+
 				if(i == 0 || j == 0) gray = 128;
 				else {
 					/*Calculate the differences between red, green, and blue values for the pixel and the
@@ -51,30 +51,30 @@ public class Image {
 					int redDiff = p.r-pixels[i-1][j-1].r;
 					int greenDiff = p.g-pixels[i-1][j-1].g;
 					int blueDiff = p.b-pixels[i-1][j-1].b;
-					
+
 					//Find the largest difference (positive or negative). We will call this maxDiff
 					if(Math.abs(redDiff) > Math.abs(maxDiff)) maxDiff = redDiff;
 					if(Math.abs(greenDiff) > Math.abs(maxDiff)) maxDiff = greenDiff;
 					if(Math.abs(blueDiff) > Math.abs(maxDiff)) maxDiff = blueDiff;
-					
+
 					/*We then add 128 to maxDiff. If there are multiple equal differences with differing signs
 					(e.g. -3 and 3), favor the red difference first, then green, then blue.*/
 					gray = 128 + maxDiff;
-					
+
 					/*If needed, we then scale gray to be between 0 and 255*/
 					if(gray < 0) gray = 0;
 					else if(gray > 255) gray = 255;
 				}
-				
-				//The pixel’s red, green, and blue values are all set to v.
+
+				//The pixels red, green, and blue values are all set to v.
 				p.r = gray;
 				p.g = gray;
 				p.b = gray;
-				
+
 			}
 		}
 	}
-	
+
 	public void blur(int blurNum) {
 		for(int i = 0; i < this.height; i++) {
 			for(int j = 0; j < this.width; j++) {
@@ -83,26 +83,26 @@ public class Image {
 				int avgG = 0;
 				int avgB = 0;
 				int numOfPixels = blurNum;
-				
+
 				for(int n = 0; n < blurNum; n++) {
 					if(j+n >= this.width) {
 						numOfPixels = n;
 						break;
 					}
-					
+
 					avgR += this.pixels[i][j+n].r;
 					avgG += this.pixels[i][j+n].g;
 					avgB += this.pixels[i][j+n].b;
 				}
-				
+
 				p.r = avgR/numOfPixels;
 				p.g = avgG/numOfPixels;
-				p.b = avgB/numOfPixels;				
-				
+				p.b = avgB/numOfPixels;
+
 			}
 		}
 	}
-	
+
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("P3\n");
@@ -114,34 +114,34 @@ public class Image {
 		}
 		return builder.toString();
 	}
-	
+
 	/*public static void main(String args[]) { //TEST
 		Pixel p1 = new Pixel(0, 240, 127);
 		Pixel p2 = new Pixel(25, 230, 122);
 		Pixel p3 = new Pixel(0, 0, 0);
 		Pixel p4 = new Pixel(255, 255, 255);
-		
+
 		Pixel p5 = new Pixel(0, 240, 127);
 		Pixel p6 = new Pixel(25, 230, 122);
 		Pixel p7 = new Pixel(0, 0, 0);
 		Pixel p8 = new Pixel(255, 255, 255);
-		
+
 		Image img1 = new Image(2,2);
 		img1.pixels[0][0] = p1;
 		img1.pixels[0][1] = p2;
 		img1.pixels[1][0] = p3;
 		img1.pixels[1][1] = p4;
-		
+
 		Image img2 = new Image(2,2);
 		img2.pixels[0][0] = p5;
 		img2.pixels[0][1] = p6;
 		img2.pixels[1][0] = p7;
 		img2.pixels[1][1] = p8;
-		
+
 		img1.emboss();
-		img2.blur(2); 
+		img2.blur(2);
 		System.out.println(img1.toString());
 		System.out.println(img2.toString());
-	
+
 	}*/
 }
--- ImageEditor.java
+++ ImageEditor.java
@@ -1,65 +1,64 @@
-import java.io.File;
+import java.io.*;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.nio.file.*;
+import java.util.regex.Pattern;
 
 public class ImageEditor {
 	Image img;
-	
+
 	public ImageEditor() {}
-	
-	void BuildImage(String HeightAndWidth) {
-		int height = 0;
-		int width = 0;
-		
-		for(int i = 0; i < HeightAndWidth.length(); i++) {
-			if(Character.isWhitespace(HeightAndWidth.charAt(i))) {
-				width = Integer.parseInt(HeightAndWidth.substring(0, i));
-				height = Integer.parseInt(HeightAndWidth.substring(i+1, HeightAndWidth.length()));
-			}
-		}
-		
-		this.img = new Image(width, height);
-	}
-	
+
 	Pixel BuildPixel(int r, int g, int b) {
 		Pixel p = new Pixel(r, g, b);
 		return p;
 	}
-		
+
 	void importFile(String fileName) {
 		try {
 			File file = new File(fileName);
             Scanner in = new Scanner(file);
-            String line = in.nextLine();
-            String HeightAndWidth = "";
-            int rgbCount = 0;
-            int r = 0;
-            int g = 0;
-            int b = 0;
-            
-            while(in.hasNextLine()) { //grab height and width
-            	line = in.nextLine();
-            	if(!line.substring(0, 1).equals("#")) {
-            		HeightAndWidth = line;
-            		line = in.nextLine(); // jump to the max color value
-            		break;
-            	}
-            }
-            
-            this.BuildImage(HeightAndWidth);
-            
-            while (in.hasNextLine()) {
-                line = in.nextLine();
-                
-                if(line.substring(0, 1).equals("#")) continue;
-                
-                rgbCount++;
-                if(rgbCount == 1) r = Integer.parseInt(line);
-                else if(rgbCount == 2) g = Integer.parseInt(line);
+            String str = in.next();
+						int width = 0;
+						int height = 0;
+
+						while (in.hasNext()) {
+							str = in.next();
+							if(str.substring(0, 1).equals("#")){
+								str = in.nextLine();
+								continue;
+							}
+							if(width == 0) width = Integer.parseInt(str);
+							else{
+								height = Integer.parseInt(str);
+								break;
+							}
+						}
+
+						this.img = new Image(width, height);
+
+						int rgbCount = 0;
+						int r = 0;
+						int g = 0;
+						int b = 0;
+
+					  str = in.next();
+
+            while (in.hasNext()) {
+                str = in.next();
+
+								if(str.substring(0, 1).equals("#")){
+									str = in.nextLine();
+									continue;
+								}
+								rgbCount++;
+
+                if(rgbCount == 1) r = Integer.parseInt(str);
+                else if(rgbCount == 2) g = Integer.parseInt(str);
                 else if(rgbCount == 3) {
-                	b = Integer.parseInt(line); 
-                	this.img.addPixel(BuildPixel(r, g, b));                	
+                	b = Integer.parseInt(str);
+                	this.img.addPixel(BuildPixel(r, g, b));
                 	rgbCount = 0;
                 }
             }
@@ -68,20 +67,20 @@ public class ImageEditor {
             ex.printStackTrace();
         }
 	}
-	
+
 	void exportFile(String fileName) {
 		try {
 			File file = new File(fileName);
-			PrintWriter out;		
+			PrintWriter out;
 			out = new PrintWriter(file);
 			out.print(this.img.toString());
-			out.close();			
+			out.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+
 		}
 	}
-	
+
 	public static void main(String args[]) {
 		ImageEditor imgEd = new ImageEditor();
 		imgEd.importFile(args[0]);
